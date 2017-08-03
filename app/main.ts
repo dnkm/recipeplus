c0ee8cafcd0857a02c07bc06d74c7f5d5e7fe25c
daniel kim <daniel.c.kim@live.com>
2017-08-02 22:41:58 -0700

    read shared info!!!

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,25 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
+import app = require('application');
+import "reflect-metadata";
+
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 import { AppModule } from "./app.module";
+
+app.on(app.launchEvent, function (args: app.ApplicationEventData) {
+    console.log("-------------------------------------");
+
+    let intent = args.android;
+    if (intent) {
+        let sub = intent.getStringExtra(android.content.Intent.EXTRA_SUBJECT);
+        let txt = intent.getStringExtra(android.content.Intent.EXTRA_TEXT);
+        console.log(sub);
+        console.log(txt);
+    }
+
+
+    console.log("-------------------------------------");
+})
 
 // A traditional NativeScript application starts by initializing global objects, setting up global CSS rules, creating, and navigating to the main page. 
 // Angular applications need to take care of their own initialization: modules, components, directives, routes, DI providers. 
